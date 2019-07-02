c6a6462a9dffb44f7656aaaab8593870d3d0ab35
dfpopov <dmytro.popov@era.io>
2019-07-02 07:31:00 +0300

    Added list ingredients feature.

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -1,11 +1,21 @@
+
+from unittest.mock import patch
+
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 
+from core import models
 
-class ModelTest(TestCase):
+
+def sample_user(email='test@era.io', password='testpass'):
+    """Create a sample user"""
+    return get_user_model().objects.create_user(email, password)
+
+
+class ModelTests(TestCase):
 
     def test_create_user_with_email_successful(self):
-        """Test creating a new user within an email is successful"""
+        """Test creating a new user with an email is successful"""
         email = 'test@era.io'
         password = 'Testpass123'
         user = get_user_model().objects.create_user(
@@ -18,8 +28,8 @@ class ModelTest(TestCase):
 
     def test_new_user_email_normalized(self):
         """Test the email for a new user is normalized"""
-        email = 'test@ERA.IO'
-        user = get_user_model().objects.create_user(email, 'Testpass123')
+        email = 'test@era.io'
+        user = get_user_model().objects.create_user(email, 'test123')
 
         self.assertEqual(user.email, email.lower())
 
@@ -37,3 +47,42 @@ class ModelTest(TestCase):
 
         self.assertTrue(user.is_superuser)
         self.assertTrue(user.is_staff)
+
+    def test_tag_str(self):
+        """Test the tag string representation"""
+        tag = models.Tag.objects.create(
+            user=sample_user(),
+            name='Vegan'
+        )
+
+        self.assertEqual(str(tag), tag.name)
+
+    def test_ingredient_str(self):
+        """Test the ingredient string respresentation"""
+        ingredient = models.Ingredient.objects.create(
+            user=sample_user(),
+            name='Cucumber'
+        )
+
+        self.assertEqual(str(ingredient), ingredient.name)
+
+    def test_recipe_str(self):
+        """Test the recipe string representation"""
+        recipe = models.Recipe.objects.create(
+            user=sample_user(),
+            title='Steak and mushroom sauce',
+            time_minutes=5,
+            price=5.00
+        )
+
+        self.assertEqual(str(recipe), recipe.title)
+
+    @patch('uuid.uuid4')
+    def test_recipe_file_name_uuid(self, mock_uuid):
+        """Test that image is saved in the correct location"""
+        uuid = 'test-uuid'
+        mock_uuid.return_value = uuid
+        file_path = models.recipe_image_file_path(None, 'myimage.jpg')
+
+        exp_path = f'uploads/recipe/{uuid}.jpg'
+        self.assertEqual(file_path, exp_path)
